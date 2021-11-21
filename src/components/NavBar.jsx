3755caa05146a33e60a8d2f2ad920633fbf4f9a4
raffael marinho <silvamarinho10@gmail.com>
2021-11-21 20:09:06 -0300

    feat: requeriment 5 done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div>
-      <Link data-testid="link-to-search" to="/search">search</Link>
-      <Link data-testid="link-to-favorites" to="/favorites">favorites</Link>
-      <Link data-testid="link-to-profile" to="/profile">profile</Link>
+      <Link data-testid="link-to-search" to="/search">pesquisar</Link>
+
+      <Link data-testid="link-to-favorites" to="/favorites">favoritas</Link>
+
+      <Link data-testid="link-to-profile" to="/profile">perfil</Link>
     </div>
   );
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 
 export default function Search() {
+  const [musica, setMusica] = useState('');
+  const valor2 = 2;
+
   return (
     <div data-testid="page-search">
       <Header />
+      <form action="">
+        <input
+          type="text"
+          value={ musica }
+          onChange={ (event) => setMusica(event.target.value) }
+          data-testid="search-artist-input"
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ musica.length < valor2 }
+        >
+          pesquisar
+        </button>
+      </form>
     </div>
   );
 }
