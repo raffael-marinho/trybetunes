ef51469492d5ddb760c42796e12c9778365daea1
raffael marinho <silvamarinho10@gmail.com>
2021-11-21 18:45:02 -0300

    feat: requeriment 2 done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
 
 export default function Login() {
-  return <div data-testid="page-login">Login</div>;
+  const [name, setName] = useState('');
+  const [wait, setWait] = useState(false);
+  const valor3 = 3;
+  const mensagemLoading = 'Carregando...';
+  const history = useHistory();
+  async function loading() {
+    setWait(true);
+    await createUser({ name });
+    setWait(false);
+    history.push('/search');
+  }
+  return (
+    <div data-testid="page-login">
+      <form action="">
+        <label htmlFor="name">
+          digite seu nome
+          <input
+            type=""
+            id="name"
+            value={ name }
+            onChange={ (event) => setName(event.target.value) }
+            data-testid="login-name-input"
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ name.length < valor3 }
+            onClick={ loading }
+          >
+            Entrar
+          </button>
+        </label>
+        {wait ? <p>{mensagemLoading}</p> : null}
+      </form>
+    </div>
+  );
 }
